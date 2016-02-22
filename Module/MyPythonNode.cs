ab0e4fde20b61b5024906df4076e57e33eead784
Karol Kuna <karol.kuna@keenswh.com>
2016-02-22 09:27:14 +0100

    mb count fix

--- Module/MyPythonNode.cs
+++ Module/MyPythonNode.cs
@@ -366,7 +366,7 @@ namespace GoodAI.Modules.Scripting
                     {
                         MyMemoryBlock<float> mb = MyMemoryManager.Instance.CreateMemoryBlock<float>(this);
                         mb.Name = "Output_" + (i + 1);
-                        mb.Count = -1;
+                        mb.Count = 0;
                         m_outputs[i] = mb;
                     }
                 }
